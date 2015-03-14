b970ca432cac0cda092e7ce0e8bde253ee3fd7ef
Manish Basantani <manu.basantani@gmail.com>
2015-03-14 07:58:00 +0000

    Setting up Prism framework. Included nuget packages for prism/unity.

---
+++ Pats/DevDumps.WPFSDK.Base/Shell/AppBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DevDumps.WPFSDK.Base.Shell
+{
+    public class AppBase : Application
+    {
+        private BootstrapperBase _bootstrapperBase;
+
+        public AppBase(BootstrapperBase bootstrapperBase)
+        {
+            _bootstrapperBase = bootstrapperBase;
+        }
+    }
+}
---
+++ Pats/DevDumps.WPFSDK.Base/Shell/BootstrapperBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Practices.Prism.UnityExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevDumps.WPFSDK.Base.Shell
+{
+    public abstract class BootstrapperBase : UnityBootstrapper 
+    {
+
+    }
+}
